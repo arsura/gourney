a2396c94908bedfa3e30aae2fd3064a3d69d08d3
Siwakorn (Si) Ruenrit <arsurarizen@gmail.com>
2021-05-23 03:39:08 +0700

    test: insert currency happy case

--- pkg/models/pgsql/currencies_test.go
+++ pkg/models/pgsql/currencies_test.go
@@ -1,36 +1,39 @@
 package pgsql
 
 import (
-	"fmt"
 	"testing"
 
+	"github.com/jackc/pgconn"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
 func TestDB_Insert(t *testing.T) {
 	mockDbConn := new(MockDBConn)
+	stmt := "INSERT INTO currencies(name, amount, total, rise_rate, rise_factor) VALUES($1, $2, $3, $4, $5)"
 	mockDbConn.On(
 		"Exec",
 		mock.Anything,
-		"INSERT INTO currencies(name, amount, total, rise_rate, rise_factor) VALUES($1, $2, $3, $4, $5)",
-		"SI",
+		stmt,
+		"RSI",
 		1000.0,
 		1000.0,
 		0.1,
 		10.0,
-	).Return(nil, nil)
+	).Return(pgconn.CommandTag("INSERT 0 1"), nil)
 
 	db := &DB{Conn: mockDbConn}
 
 	result, err := db.Insert(
 		&Currency{
-			Name:       "SI",
+			Name:       "RSI",
 			Amount:     1000.0,
 			Total:      1000.0,
 			RiseRate:   0.1,
 			RiseFactor: 10.0,
 		},
 	)
-	fmt.Println(result.RowsAffected, err)
-	// dbConn.AssertExpectations(t)
+	assert.Equal(t, result, int64(1))
+	assert.Nil(t, err)
+
 }
