f1abfc6cf5a33dbe042f2f74cf8892354534efef
SIWAKORN <arsurarizen@gmail.com>
2023-01-11 17:52:40 +0700

    fix: validator

--- pkg/validator/validator.go
+++ pkg/validator/validator.go
@@ -6,18 +6,18 @@ import (
 
 	"github.com/go-playground/locales/en"
 	ut "github.com/go-playground/universal-translator"
-	gpg_validator "github.com/go-playground/validator/v10"
-	en_translations "github.com/go-playground/validator/v10/translations/en"
+	gpgvalidator "github.com/go-playground/validator/v10"
+	entranslations "github.com/go-playground/validator/v10/translations/en"
 )
 
 type Validator struct {
-	Validate *gpg_validator.Validate
+	Validate *gpgvalidator.Validate
 	Trans    ut.Translator
 }
 
 func (v *Validator) TransError(err error) []string {
 	result := []string{}
-	errs := err.(gpg_validator.ValidationErrors)
+	errs := err.(gpgvalidator.ValidationErrors)
 	for _, e := range errs {
 		result = append(result, e.Translate(v.Trans))
 	}
@@ -28,8 +28,8 @@ func NewValidator() *Validator {
 	en := en.New()
 	uni := ut.New(en, en)
 	trans, _ := uni.GetTranslator("en")
-	validator := gpg_validator.New()
-	en_translations.RegisterDefaultTranslations(validator, trans)
+	validator := gpgvalidator.New()
+	entranslations.RegisterDefaultTranslations(validator, trans)
 	validator.RegisterTagNameFunc(func(fld reflect.StructField) string {
 		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
 		if name == "-" {
